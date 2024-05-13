49e49e97715edef6261660ff415e66f805a7f5fa
Diogo Oliveira Mascarenhas <diogomascarenha@gmail.com>
2024-05-13 19:12:40 -0300

    Adicionando Laravel Passport

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,5 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        Passport::hashClientSecrets();
+        Passport::enablePasswordGrant();
     }
 }
